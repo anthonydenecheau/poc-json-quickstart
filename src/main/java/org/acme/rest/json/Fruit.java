ada6653995e8221e64f33bb4e1c0f137d9ac4667
anthonydenecheau <anthony.denecheau@centrale-canine.fr>
2020-06-12 14:39:08 +0200

    :worker_construction:

--- src/main/java/org/acme/rest/json/Fruit.java
+++ src/main/java/org/acme/rest/json/Fruit.java
@@ -1,15 +1,22 @@
 package org.acme.rest.json;
 
-public class Fruit {
+import javax.persistence.Cacheable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
 
+import io.quarkus.hibernate.orm.panache.PanacheEntity;
+
+@Entity
+@Cacheable
+public class Fruit extends PanacheEntity {
+
+    @Column(length = 40, unique = true)
     public String name;
-    public String description;
 
     public Fruit() {
     }
 
-    public Fruit(String name, String description) {
+    public Fruit(String name) {
         this.name = name;
-        this.description = description;
     }
 }
--- src/main/java/org/acme/rest/json/FruitResource.java
+++ src/main/java/org/acme/rest/json/FruitResource.java
@@ -1,44 +1,106 @@
 package org.acme.rest.json;
 
-import java.util.Collections;
-import java.util.LinkedHashMap;
-import java.util.Set;
 
+package org.acme.hibernate.orm.panache;
+
+import java.util.List;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.json.Json;
+import javax.transaction.Transactional;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
+
+import org.jboss.resteasy.annotations.jaxrs.PathParam;
+
+import io.quarkus.panache.common.Sort;
 
 @Path("/api/fruits")
-@Produces(MediaType.APPLICATION_JSON)
-@Consumes(MediaType.APPLICATION_JSON)
+@ApplicationScoped
+@Produces("application/json")
+@Consumes("application/json")
 public class FruitResource {
 
-    private Set<Fruit> fruits = Collections.newSetFromMap(Collections.synchronizedMap(new LinkedHashMap<>()));
-
-    public FruitResource() {
-        fruits.add(new Fruit("Apple", "Winter fruit"));
-        fruits.add(new Fruit("Pineapple", "Tropical fruit"));
+    @GET
+    @Path("/findAll")	
+    public List<Fruit> get() {
+        return Fruit.listAll(Sort.by("name"));
     }
 
     @GET
-    @Path("/findAll")
-    public Set<Fruit> list() {
-        return fruits;
+    @Path("{id}")
+    public Fruit getSingle(@PathParam Long id) {
+        Fruit entity = Fruit.findById(id);
+        if (entity == null) {
+            throw new WebApplicationException("Fruit with id of " + id + " does not exist.", 404);
+        }
+        return entity;
     }
 
     @POST
-    public Set<Fruit> add(Fruit fruit) {
-        fruits.add(fruit);
-        return fruits;
+    @Transactional
+    public Response create(Fruit fruit) {
+        if (fruit.id != null) {
+            throw new WebApplicationException("Id was invalidly set on request.", 422);
+        }
+
+        fruit.persist();
+        return Response.ok(fruit).status(201).build();
+    }
+
+    @PUT
+    @Path("{id}")
+    @Transactional
+    public Fruit update(@PathParam Long id, Fruit fruit) {
+        if (fruit.name == null) {
+            throw new WebApplicationException("Fruit Name was not set on request.", 422);
+        }
+
+        Fruit entity = Fruit.findById(id);
+
+        if (entity == null) {
+            throw new WebApplicationException("Fruit with id of " + id + " does not exist.", 404);
+        }
+
+        entity.name = fruit.name;
+
+        return entity;
     }
 
     @DELETE
-    public Set<Fruit> delete(Fruit fruit) {
-        fruits.removeIf(existingFruit -> existingFruit.name.contentEquals(fruit.name));
-        return fruits;
+    @Path("{id}")
+    @Transactional
+    public Response delete(@PathParam Long id) {
+        Fruit entity = Fruit.findById(id);
+        if (entity == null) {
+            throw new WebApplicationException("Fruit with id of " + id + " does not exist.", 404);
+        }
+        entity.delete();
+        return Response.status(204).build();
+    }
+
+    @Provider
+    public static class ErrorMapper implements ExceptionMapper<Exception> {
+
+        @Override
+        public Response toResponse(Exception exception) {
+            int code = 500;
+            if (exception instanceof WebApplicationException) {
+                code = ((WebApplicationException) exception).getResponse().getStatus();
+            }
+            return Response.status(code)
+                    .entity(Json.createObjectBuilder().add("error", exception.getMessage()).add("code", code).build())
+                    .build();
+        }
+
     }
 }

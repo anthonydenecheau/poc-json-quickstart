cc66dd3aae7e2e59a3f810f0ce45f08844caf7f9
anthonydenecheau <anthony.denecheau@centrale-canine.fr>
2020-06-14 01:53:41 +0200

    :construction_worker:

--- src/main/script/run.sh
+++ src/main/script/run.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 
 # Start the proxy
+echo "Start the proxy"
 /usr/local/bin/cloud_sql_proxy -instances=${DATASOURCE_URL}=tcp:5432 -credential_file=ws-openid.json &
 
 # wait for the proxy to spin up
 sleep 10
 
 # Start the server
+echo "Start the server"
 ./application -Dquarkus.http.host=0.0.0.0 -Dquarkus.http.port=$PORT -Djavax.net.ssl.trustStore=/work/cacerts

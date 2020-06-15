81d577f5bea0a35186adffb2246378a312d7ab89
anthonydenecheau <anthony.denecheau@centrale-canine.fr>
2020-06-15 10:05:05 +0200

    :construction_worker:

--- src/main/script/run.sh
+++ src/main/script/run.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
 ## Start the proxy
-# fix: cf. https://github.com/quarkusio/quarkus/issues/9985
-#echo "Start the proxy"
-#/usr/local/bin/cloud_sql_proxy -instances=${DATASOURCE_URL}=tcp:5432 -credential_file=ws-openid.json &
+# pending fix : cf. https://github.com/quarkusio/quarkus/issues/9985
+echo "Start the proxy"
+/usr/local/bin/cloud_sql_proxy -instances=${DATASOURCE_URL}=tcp:5432 -credential_file=ws-openid.json &
 
 # wait for the proxy to spin up
-#sleep 10
+sleep 10
 
 ## Start the server
 echo "Start the server"
